83a7cf00cc9de1b04d44385ff5b7782d95291574
onishi taisei <taisei.onishi@axia-works.com>
2022-05-24 22:03:04 +0900

    create TetrisGame class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tetris_game/tetris_game.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TetrisGame(),
     );
   }
 }
---
+++ lib/tetris_game.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TetrisGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
